e527b23c1ac52583673a3335c8d2abca9fdb0a74
Matan Shavit <71092861+matanshavit@users.noreply.github.com>
2023-03-12 15:56:31 -0400

    find all valid placements of pieces in an empty board

--- exact_cover.py
+++ exact_cover.py
@@ -249,3 +249,55 @@ def translate_piece_to(piece, new_coordinate):
         (coordinate[0] + delta_row, coordinate[1] + delta_column)
         for coordinate in piece
     ]
+
+
+def is_piece_in_board(board_squares, piece_squares):
+    return all(piece_square in board_squares for piece_square in piece_squares)
+
+
+# get every placement of a piece on an empty board
+# by translating the first square of the piece given to
+# each empty square of the board, and checking if the rest of the piece is still in the board
+def get_piece_placements(board_squares):
+    def get_piece_orientations_placements(piece_orientations):
+        result = []
+        for piece_orientation in piece_orientations:
+            for square in board_squares:
+                translated_piece = translate_piece_to(piece_orientation, square)
+                if is_piece_in_board(board_squares, translated_piece):
+                    result.append(translated_piece)
+        return result
+                
+
+    return [
+        get_piece_orientations_placements(piece_orientations)
+        for piece_orientations in PIECE_ORIENTATION_SQUARES
+    ]
+
+# example:
+# board_squares = get_calendar_squares('Mar', '12', 'Sun')
+# placements = get_piece_placements(board_squares)
+# lengths = [len(placement_options) for placement_options in placements]
+# print(lengths)
+# print(sum(lengths))
+
+
+def get_every_placement_for_every_day():
+    for month in [
+        'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec',
+    ]:
+        for day in [
+            'Sun', 'Mon', 'Tues', 'Wed', 'Thur', 'Fri', 'Sat'
+        ]:
+            for date in [
+                "1", "2", "3", "4", "5", "6", "7", "8", "9", "10",
+                "11", "12", "13", "14", "15", "16", "17", "18", "19", "20",
+                "21", "22", "23", "24", "25", "26", "27", "28", "29", "30",
+                "31",         
+            ]:
+                board_squares = get_calendar_squares(month, date, day)
+                placements = get_piece_placements(board_squares)
+                lengths = [len(placement_options) for placement_options in placements]
+
+# timed - 24 seconds for every possible puzzle definition (combination of 3 squares)
+# get_every_placement_for_every_day()
