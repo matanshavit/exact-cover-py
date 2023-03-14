8bb76bac50e3b653bf839aeec4a17126bef0371a
Matan Shavit <71092861+matanshavit@users.noreply.github.com>
2023-03-13 23:25:32 -0400

    algorithm to find valid arrangements

--- exact_cover.py
+++ exact_cover.py
@@ -1,3 +1,5 @@
+import random
+
 from calendar_squares import get_calendar_squares
 from pieces import PIECE_ORIENTATION_SQUARES
 
@@ -29,7 +31,6 @@ def get_piece_placements(board_squares):
                 if is_piece_in_board(board_squares, translated_piece):
                     result.append(translated_piece)
         return result
-                
 
     return [
         get_piece_orientations_placements(piece_orientations)
@@ -86,10 +87,87 @@ def get_compatabile_placement_indicies(piece_placements):
 # print(sum(lengths))
 
 
-def placements_and_compatability(month, date, day):
+# return valid arrangements of piece placements without overlaps
+# return value is a list of placement indices in order of piece index
+# for example, a return value of [10, 11, 12, ...]
+# represents placements [0][10], [1][11], [2][12], ...
+
+def get_valid_arrangements(compatible_indices, return_first, random_order):
+    last_piece_index = len(compatible_indices) - 1
+
+    def _get_valid_arrangements(partial_arrangement, remaining_possible_placement_indices):
+        full_arrangements = []
+        next_piece_index = len(partial_arrangement)
+        pieces_compatible_indices = compatible_indices[next_piece_index]
+        if random_order:
+            random.shuffle(remaining_possible_placement_indices[0])
+        for next_placement_index in remaining_possible_placement_indices[0]:
+            new_arrangement = partial_arrangement + [next_placement_index]
+
+            # print progress tracking by first and second piece
+            if next_piece_index == 1:
+                print(new_arrangement)
+
+            if next_piece_index == last_piece_index:
+                # the arrangement is done and valid
+                print(new_arrangement)
+                full_arrangements.append(new_arrangement)
+                if return_first:
+                    return full_arrangements
+                continue
+
+            placement_compatible_indices = pieces_compatible_indices[next_placement_index]
+            next_possible_placements = []
+            for future_piece_index, future_placement_indicies in enumerate(
+                remaining_possible_placement_indices[1:], next_piece_index + 1
+            ):
+                next_possible_placements.append([
+                    future_placement_index
+                    for future_placement_index in future_placement_indicies
+                    if placement_compatible_indices[(future_piece_index, future_placement_index)]
+                ])
+            if any(
+                len(next_possible_placements_for_piece) == 0
+                for next_possible_placements_for_piece in next_possible_placements
+            ):
+                # if placements eliminate all possiblities for a future piece,
+                # there is no valid arrangement for this placement
+                continue
+
+            full_arrangements = full_arrangements + _get_valid_arrangements(new_arrangement, next_possible_placements)
+            if return_first and len(full_arrangements) > 0:
+                return full_arrangements
+
+        return full_arrangements
+
+    possible_placement_indices = [
+        list(range(len(possible_placement_indices)))
+        for possible_placement_indices in compatible_indices
+    ]
+    return _get_valid_arrangements([], possible_placement_indices)
+
+
+def find_arrangements_for_date(month, date, day, return_first=True, random_order=True):
     board_squares = get_calendar_squares(month, date, day)
     piece_placements = get_piece_placements(board_squares)
     compatible_indices = get_compatabile_placement_indicies(piece_placements)
+    valid_arrangements = get_valid_arrangements(compatible_indices, return_first, random_order)
+    return [
+        [
+            piece_placements[piece][placement]
+            for piece, placement in enumerate(arrangement)
+        ]
+        for arrangement in valid_arrangements
+    ]
 
+def print_arrangements_for_date(month, date, day):
+    arrangements = find_arrangements_for_date(month, date, day)
+    if len(arrangements) > 1:
+        print()
+        print(f'{len(arrangements)} valid arrangements')
+    for arrangement in arrangements:
+        print()
+        for placement in arrangement:
+            print(placement)
 
-placements_and_compatability("Mar", "12", "Sun")
+print_arrangements_for_date("Mar", "14", "Tues")
